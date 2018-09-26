7158594099ec9c76c45ff1a6ebd22e560ee2df21
Papa-Victor <vinceplourd@gmail.com>
2018-09-26 14:47:08 -0400

    Utils.h added function
    Added StringXOR to Utils.h
    Does XOR on 2 string, if not same length: return "". else return XOR

--- Travail1/Travail1/Utils.h
+++ Travail1/Travail1/Utils.h
@@ -2,7 +2,9 @@
 #define UTILS_H
 
 #include <array>
+using std::string;
 
+//rotates an array one index to the right
 template <class T, size_t SIZE>
 void RotateArrayRight(std::array<T, SIZE> &rArray) {
 	int size = rArray.size();
@@ -13,9 +15,12 @@ void RotateArrayRight(std::array<T, SIZE> &rArray) {
 	rArray[0] = last;
 
 }
-
+//rotates an array numRotations indexes to the right
 template <class T, size_t SIZE>
 void RotateArrayRight(std::array<T,SIZE> &rArray, int numRotations) {
+	if (numRotations < 0) {
+		return;
+	}
 	int size = rArray.size();
 	for (int j = 0; j < numRotations; j++) {
 		T last = rArray[size - 1];
@@ -25,6 +30,18 @@ void RotateArrayRight(std::array<T,SIZE> &rArray, int numRotations) {
 		rArray[0] = last;
 	}
 
+}
+
+string StringXOR(string string1, string string2) {
+	int string1l = string1.length();
+	if (string1l != string2.length()) {
+		return "";
+	}
+	string returnString = "";
+	for (int i = 0; i < string1l; i++) {
+		returnString += string1[i] ^ string2[i];
+	}
+	return returnString;
 }
 
 #endif // !UTILS_H
